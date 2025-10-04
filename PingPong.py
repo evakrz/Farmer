28d0cc09bb6ed4c2d8fd7626e9f2d5450f66a025
Eva Krzisnik <eva.krzisnik@gmail.com>
2025-10-04 23:14:32 +0200

    commit

--- PingPong.py
+++ PingPong.py
@@ -1,69 +1,91 @@
 def AutoDinosPingPong(max_bones):
-	clear()
-	change_hat(Hats.Dinosaur_Hat)
+	while num_items(Items.Bone) < max_bones:
+		clear()
+		change_hat(Hats.Dinosaur_Hat)
 
-	on_left = True
+		on_left = True
 
-	# Find first apple by scanning
-	apple_x = None
-	apple_y = None
-	for x in range(get_world_size()):
-		for y in range(get_world_size()):
-			if get_entity_type() == Entities.Apple:
-				# Measure to get next apple position
-				next_apple = measure()
-				if next_apple != None:
-					apple_x, apple_y = next_apple
+		# Find first apple by scanning
+		apple_x = None
+		apple_y = None
+		for x in range(get_world_size()):
+			for y in range(get_world_size()):
+				if get_entity_type() == Entities.Apple:
+					# Measure to get next apple position
+					next_apple = measure()
+					if next_apple != None:
+						apple_x, apple_y = next_apple
+						break
+				if not move(North):
+					change_hat(Hats.Straw_Hat)
 					break
-			move(North)
-		if apple_x != None:
-			break
-		move(East)
+			if apple_x != None:
+				break
+			if not move(East):
+				change_hat(Hats.Straw_Hat)
+				break
 
-	while num_items(Items.Bone) < max_bones and apple_x != None:
-		# Move to the edge column (left or right)
-		if on_left:
-			# Go to leftmost column (x=0)
-			while get_pos_x() != 0:
-				move(West)
-		else:
-			# Go to rightmost column
-			while get_pos_x() != get_world_size() - 1:
-				move(East)
-
-		# Move to apple's Y position
-		while get_pos_y() != apple_y:
-			if get_pos_y() < apple_y:
-				move(North)
+		# Ping-pong loop
+		while num_items(Items.Bone) < max_bones and apple_x != None:
+			# Move to the edge column (left or right)
+			if on_left:
+				# Go to leftmost column (x=0)
+				while get_pos_x() != 0:
+					if not move(West):
+						change_hat(Hats.Straw_Hat)
+						break
 			else:
-				move(South)
+				# Go to rightmost column
+				while get_pos_x() != get_world_size() - 1:
+					if not move(East):
+						change_hat(Hats.Straw_Hat)
+						break
 
-		# Sweep across until we reach the apple
-		if on_left:
-			# Sweep right
-			while get_pos_x() != apple_x:
-				move(East)
-		else:
-			# Sweep left
-			while get_pos_x() != apple_x:
-				move(West)
+			# Move to apple's Y position
+			while get_pos_y() != apple_y:
+				if get_pos_y() < apple_y:
+					if not move(North):
+						change_hat(Hats.Straw_Hat)
+						break
+				else:
+					if not move(South):
+						change_hat(Hats.Straw_Hat)
+						break
 
-		# Now we're on the apple - measure next apple position
-		next_apple = measure()
-		if next_apple != None:
-			apple_x, apple_y = next_apple
-		else:
-			apple_x = None
+			# Sweep across until we reach the apple
+			if on_left:
+				# Sweep right
+				while get_pos_x() != apple_x:
+					if not move(East):
+						change_hat(Hats.Straw_Hat)
+						break
+			else:
+				# Sweep left
+				while get_pos_x() != apple_x:
+					if not move(West):
+						change_hat(Hats.Straw_Hat)
+						break
 
-		# Continue to edge
-		if on_left:
-			while get_pos_x() < get_world_size() - 1:
-				move(East)
-		else:
-			while get_pos_x() > 0:
-				move(West)
+			# Now we're on the apple - measure next apple position
+			next_apple = measure()
+			if next_apple != None:
+				apple_x, apple_y = next_apple
+			else:
+				apple_x = None
 
-		# Toggle side for next iteration
-		on_left = not on_left
+			# Continue to edge
+			if on_left:
+				while get_pos_x() < get_world_size() - 1:
+					if not move(East):
+						change_hat(Hats.Straw_Hat)
+						break
+			else:
+				while get_pos_x() > 0:
+					if not move(West):
+						change_hat(Hats.Straw_Hat)
+						break
+
+			# Toggle side for next iteration
+			on_left = not on_left
 
 	change_hat(Hats.Straw_Hat)
